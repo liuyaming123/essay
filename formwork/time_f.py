acd23fdd406a3d590f1aa579699c76a86932c65d
liuyaming <lym_6017@163.com>
2020-05-20 15:03:21 +0800

    adds

--- formwork/time_f.py
+++ formwork/time_f.py
@@ -3,7 +3,6 @@
 import datetime
 import time
 
-
 # 获取现在的时间
 a = datetime.datetime.now()
 # print(a.__format__("%Y-%m-%d %H:%M:%S"))  # 与此方法等价的方法为strftime(...)
@@ -21,12 +20,13 @@ c = (datetime.date.today().year, datetime.date.today().month, datetime.date.toda
 
 # 计算时间差，可以计算: 天(days), 小时(hours), 分钟(minutes), 秒(seconds), 微秒(microseconds).
 d1 = datetime.datetime.now()
+
+
 # d2 = d1 + datetime.timedelta(hours=8)
 # d2 = d1 - datetime.timedelta(days=2)
 # d2 = d2.strftime("%Y-%m-%d")
 # print(d2)
 # print(type(d2))
-
 
 
 def get_changed_date(change, format_, sources_time=None):
@@ -60,11 +60,11 @@ def get_changed_time(change, format_, sources_time=None):
     return ti
 
 
-
-
 # pre_3_days = int(get_changed_time(- 3 * 24 * 60 * 60, "%Y%m%d %H%M%S"))
 # pre_3_days = int(get_changed_time(- 3 * 24 * 60 * 60, "%Y%m%d %H%M%S"))
 pre_3_days = int(get_changed_time(- 3 * 24 * 60 * 60, "%Y%m%d %H%M%S"))
+
+
 # print(pre_3_days)
 
 
@@ -75,4 +75,3 @@ def get_pre_time(pre_day=0):
 
 
 print(get_pre_time(-15))
-
